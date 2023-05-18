a6fb5010ce908eaf937a8cf901f9bcc431513748
dballard10 <77413794+dballard10@users.noreply.github.com>
2023-05-17 23:08:48 -0400

    made changes to main

--- RubiksCube.js
+++ RubiksCube.js
@@ -3,29 +3,46 @@ class RubiksCube {
     //Best of calculator
     //Fastest Time
     //Calculate Average
-    #times;
+    times;
     //Scrambles Algorithm
 
-
-    while (sessionrunning) {
-        getTime
-        if (#times.length >= 5) {
-            showBestOf5
-        }
-        if (this.#times.length >= 12) {
-            showBestOf12
-        }
-    }
-
-    }
-
     constructor() {
-        this.#times = []
+        this.times = []
+        main()
     }
 
-    function getTime()
+    main() {
+        var bestBO5 = 1000000000000000000
+        var bestBO12 = 1000000000000000000
+        var bestBO50 = 1000000000000000000
+        while (sessionrunning) {
+            //getTime
+            //addtime to times
+            if (times.length >= 5) {
+                //curr = showBestOf5
+                if (curr < bestBO5) {
+                    bestBO5 = curr
+                }
+            }
+            if (this.times.length >= 12) {
+                //showBestOf12
+                if (curr < bestBO12) {
+                    bestBO12 = curr
+                }
+            }
 
-    function calculateAverage(times) {
+            if (this.times.length >= 50) {
+                //showBestOf50
+                if (curr < bestBO50) {
+                    bestBO50 = curr
+                }
+            }
+        }
+    }
+
+    getTime()
+
+    calculateAverage(times) {
         let temp = times
         temp.sort()
         var sum = 0
@@ -35,11 +52,6 @@ class RubiksCube {
         var average = sum / (times.length - 2)
         print(average)
         return average
-    }
-    function bestTime(mostRecentTime)  {
-        if (mostRecentTime < bestTime)  {
-            bestTime = mostRecentTime
-        }
     }
 
 }
