ec63b0e261212121a200a591467f558769abc632
Matt Nicoud <nicoudm5@gmail.com>
2023-05-28 00:13:13 -0400

    values stored in times arr and calcAverage fixed

--- RubiksCube.js
+++ RubiksCube.js
@@ -4,92 +4,109 @@ class RubiksCube {
     //Fastest Time
     //Calculate Average
     times;
+    fastestTime;
+    bestBO5;
+    bestBO12;
+    bestBO50;
     //Scrambles Algorithm
 
     constructor() {
         this.times = []
-        main()
+        this.fastestTime = 100000000000000000
+        this.bestBO5 = 1000000000000000000
+        this.bestBO12 = 1000000000000000000
+        this.bestBO50 = 1000000000000000000
     }
-    
-    main() {
-        var fastestTime = 100000000000000000
-        var bestBO5 = 1000000000000000000
-        var bestBO12 = 1000000000000000000
-        var bestBO50 = 1000000000000000000
 
-        while (sessionrunning) {
-            time = getTime()
-            if (time < fastestTime) {
-                fastestTime = time
-            }
-            this.times.push(time)
-            if (times.length >= 5) {
-                currBO5 = bestof5average
-                if (currBO5 < bestBO5) {
-                    bestBO5 = curr
-                }
-            }
-            if (this.times.length >= 12) {
-                //showBestOf12
-                if (curr < bestBO12) {
-                    bestBO12 = curr
-                }
-            }
-
-            if (this.times.length >= 50) {
-                //showBestOf50
-                if (curr < bestBO50) {
-                    bestBO50 = curr
-                }
+    updateTimes(time) {
+        this.times.push(time)
+        if (time < this.fastestTime) {
+            this.fastestTime = time
+        }
+        if (this.times.length >= 5) {
+            //showBestOf5
+            if (time < this.bestBO5) {
+                this.bestBO5 = time
             }
         }
-    }
-
-    static getTime() {
-        return 1
+        if (this.times.length >= 12) {
+            //showBestOf12
+            if (time < this.bestBO12) {
+                this.bestBO12 = time
+            }
+        }
+        if (this.times.length >= 50) {
+            //showBestOf50
+            if (time < this.bestBO50) {
+                this.bestBO50 = time
+            }
+        }
     }
 
     calculateAverage(times) {
-        let temp = times
+        let temp = times.map((x) => (x))
         temp.sort()
-        var sum = 0
-        for(let i = 1; i < times.length - 1; i++) {
+        var sum = temp[0]
+        var average = sum
+        for(let i = 1; i < times.length; i++) {
             sum += temp[i]
         }
-        var average = sum / (times.length - 2)
-        print(average)
+        var average = sum / (times.length)
+        
+        console.log("Average: " + average)
         return average
     }
 
 }
 
-var timeElement = document.getElementById('time')
-timeElement.innerHTML = "Time: 0.00 s"
-let recordingTime = false
+let cube = new RubiksCube()
+
+let timeElement = document.getElementById('time')
+let averageElement = document.getElementById('average')
+
+let isRecordingTime = false
 let start, end
 window.addEventListener('keydown', function(e) {
     if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-        if (!recordingTime) {
+        if (!isRecordingTime) {
             start = new Date()
-            recordingTime = true
-            displayTime(recordingTime)
+            isRecordingTime = true
+            timeElement.style = "border:red; border-width:2px; padding:5px; display:inline; border-style:outset;"
+            displayRunningTime()
         } else {
             end = new Date()
             let totalTime = end.getTime() - start.getTime()
-            timeElement.innerHTML = "Time: " + totalTime + " ms"
-            recordingTime = false
-        }
+            isRecordingTime = false
+            timeElement.style="border:azure; border-width:2px; padding:5px; display:inline; border-style:outset;"
+            displayRunningTime()
+            cube.updateTimes(totalTime)
+            averageElement.innerHTML = "Average Time: " + cube.calculateAverage(cube.times)  + " ms"        }
     }
 })
-function displayTime(recording) {
-    if (recording) {
+function displayRunningTime() {
+    if (isRecordingTime) {
         let today = new Date()
         let runningTime = (today.getTime() - start.getTime())
-        if (Math.trunc(runningTime / 1000) > 0) {
-            timeElement.innerHTML = "Time: " + Math.trunc(runningTime / 1000) + "." + (runningTime % 1000) + " s"
-        } else {
-            timeElement.innerHTML = "Time: 0." + runningTime  + " s"
-        }
-        setTimeout(displayTime.bind(null, recordingTime), 1)
+        let timeString = runningTime.toString()
+        if (Math.trunc(runningTime / 1000) > 0) 
+            timeElement.innerHTML = "Current Time: " + Math.trunc(runningTime / 1000) + "." + timeString.substring(timeString.length-3) + " s"
+        else if (Math.trunc(runningTime / 100) > 0) 
+            timeElement.innerHTML = "Current Time: 0." + runningTime  + " s"
+        else if (Math.trunc(runningTime / 10) > 0) 
+            timeElement.innerHTML = "Current Time: 0.0" + runningTime  + " s"
+        else 
+            timeElement.innerHTML = "Current Time: 0.00" + runningTime  + " s"
+        setTimeout(displayRunningTime.bind(null, isRecordingTime), 1)
+    } else {
+        let runningTime = (end.getTime() - start.getTime())
+        let timeString = runningTime.toString()
+        if (Math.trunc(runningTime / 1000) > 0) 
+            timeElement.innerHTML = "Current Time: " + Math.trunc(runningTime / 1000) + "." + timeString.substring(timeString.length-3) + " s"
+        else if (Math.trunc(runningTime / 100) > 0) 
+            timeElement.innerHTML = "Current Time: 0." + runningTime  + " s"
+        else if (Math.trunc(runningTime / 10) > 0) 
+            timeElement.innerHTML = "Current Time: 0.0" + runningTime  + " s"
+        else 
+            timeElement.innerHTML = "Current Time: 0.00" + runningTime  + " s"
     }
 }
